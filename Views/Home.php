feff27e039245c7918dee8447b40d9230c690eac
QuentinDPT <quentin@depotter.fr>
2020-03-28 19:45:55 +0100

    [UPDATE] home page interractive

--- Views/Home.php
+++ Views/Home.php
@@ -1,4 +1,5 @@
 <?php
+
 // Begin session
 session_start();
 
@@ -23,7 +24,7 @@ $global_data['Followed'] = C_User::GetFollow($userConnected);
 
 
 $nb_themes_displayed = count($global_data['Themes']);
-if ($nb_themes_displayed > 3) $nb_themes_displayed = 3;
+//if ($nb_themes_displayed > 3) $nb_themes_displayed = 3;
 
 function getVideosByThemeId($list, $id) {
     $listRes = array();
@@ -33,14 +34,26 @@ function getVideosByThemeId($list, $id) {
     return $listRes;
 }
 function createVideoRec($vid) {
-    $styleDiv = "border: 1px solid black; border-radius: 10px; height: 300px; min-width: 250px; max-width: 250px; margin: 10px;";
-    $styleDiv2 = "min-width: 100%; min-height: 90%; max-height: 90%; max-width: 100%; height: 90%; background-color: #252525; border-radius: 10px 10px 0 0; position: relative; overflow: hidden";
-    $styleImg = "width: 100%; height: auto; border-radius: 10px; margin: auto; position: absolute; top: 0; bottom: 0; object-fit: cover;";
-    $div = "<div class='' style='$styleDiv' onclick='submitForm(this, `formVideo`)'>";
-    $div2 = "<div style='$styleDiv2'>";
-    $img = "<img src='" . $vid->getThumbnail() . "' alt='Thumbnail' style='$styleImg' id='" . $vid->getId() . "'>";
-    $h4 = "<h4 class='text-center'>" . $vid->getName() . "</h4>";
-    return "$div $div2 $img </div> $h4 </div>";
+    //$styleDiv = "border: 1px solid black; border-radius: 10px; height: 300px; min-width: 250px; max-width: 250px; margin: 10px;";
+    //$styleDiv2 = "min-width: 100%; min-height: 90%; max-height: 90%; max-width: 100%; height: 90%; background-color: #252525; border-radius: 10px 10px 0 0; position: relative; overflow: hidden";
+    //$styleImg = "width: 100%; height: auto; border-radius: 10px; margin: auto; position: absolute; top: 0; bottom: 0; object-fit: cover;";
+    //$div = "<div class='video' onclick='submitForm(this, `formVideo`)'>";
+    //$div2 = "<div style='$styleDiv2'>";
+    //$divDesc = "<div class='description'>" . $vid->getDescription() . "</div>";
+    //$img = "<img src='" . $vid->getThumbnail() . "' alt='Thumbnail' style='$styleImg' id='" . $vid->getId() . "'>";
+    //$h4 = "<h4 class='text-center'>" . $vid->getName() . "</h4>";
+    //return "$div $div2 $img $divDesc </div> $h4 </div>";
+
+    return
+    '<div class="video col-5 col-sm-4 col-md-2" onclick="submitForm(this, `formVideo`)">
+      <div>
+        <div class="thumbnail">
+          <img src="' . $vid->getThumbnail() .'" alt="Thumbnail" id="' . $vid->getId() . '">
+        </div>
+        <div class="description">' . $vid->getDescription() . '</div>
+      </div>
+      <h4 class="title">' . $vid->getName() . '</h4>
+    </div>' ;
 }
 
 ?>
@@ -51,6 +64,8 @@ function createVideoRec($vid) {
   <body>
       <?php require("./Views/Common/navbar.php") ?>
 
+      <link rel="stylesheet" href="/src/styles/thumbnail.css">
+
       <main class="container-fluid mb-4">
           <!-- Content =================================================== -->
           <section class="row">
@@ -60,9 +75,9 @@ function createVideoRec($vid) {
               <div class="col-10">
                   <form class="" action="/watch" method="get" id="formVideo">
                       <?php for ($i=0; $i < $nb_themes_displayed; $i++) { ?>
-                          <div class="col-md-12">
+                          <div class="theme">
                               <h2><?php echo $global_data['Themes'][$i]->getName() ?></h2>
-                              <div style="display: flex; overflow-x: scroll;">
+                              <div style="display: flex; overflow-x: auto;">
                                   <?php
                                   $filtered_list = getVideosByThemeId($global_data, $global_data['Themes'][$i]->getId());
                                   for ($j=0; $j<count($filtered_list); $j++) {
@@ -97,5 +112,11 @@ function createVideoRec($vid) {
 
           document.getElementById(formId).submit();
       }
+  </script>
+  <script type="text/javascript">
+    var sqt = document.getElementsByTagName("square") ;
+    for (var i of sqt) {
+      i.style.height = i.stle.width ;
+    }
   </script>
 </html>
