8f4a9cc54a3bfc01b5d433a45930b91249e32dc3
QuentinDPT <quentin@depotter.fr>
2020-05-19 14:27:02 +0200

    [UPDATE] Forgot password

--- Controllers/C_User.php
+++ Controllers/C_User.php
@@ -59,11 +59,14 @@ class C_User {
         $sub = "Réinitialisation de votre mot de passe" ;
         $mailContent = "Bonjour " . $user->getName() . ",\n\nVous avez demander récemment à changer votre mot de passe.\nVoici votre nouveau mot de passe : \n" . $newPassword . "\nUne fois sur votre compte, nous vous coneillons de créer un nouveau mot de passe" ;
 
-        ob_start(); //Turn on output buffering
+        $headers  = 'From: Infinite Skills <infinite.skills@quentin.depotter.fr>\r\n' ;
+        $headers .= 'MIME-Version: 1.0\r\n' ;
+        $headers .= 'Content-type: text/html; charset=iso-8859-1' ;
+
+        ob_start();
         require($_SERVER['DOCUMENT_ROOT']."/Views/Mailing.php") ;
-        //copy current buffer contents into $message variable and delete current output buffer
         $message = ob_get_clean();
-        return new Mail($dest, $sub, $message, 'MIME-Version: 1.0\r\nContent-type: text/html; charset=iso-8859-1') ;
+        return new Mail($dest, $sub, $message, $headers) ;
     }
     /* GetUsers: Get all users from database
      *      Output:
--- Views/Mailing.php
+++ Views/Mailing.php
@@ -1,17 +1,4 @@
-
---PHP-alt-<?php echo $random_hash; ?>
-Content-Type: text/plain; charset="iso-8859-1"
-Content-Transfer-Encoding: 7bit
-
-Hello World!!!
-This is simple text email message.
-
---PHP-alt-<?php echo $random_hash; ?>
-Content-Type: text/html; charset="iso-8859-1"
-Content-Transfer-Encoding: 7bit
-
-<h2>Hello World!</h2>
-<p>This is something with <b>HTML</b> formatting.</p>
-<?= $mailContent ?>
-
---PHP-alt-<?php echo $random_hash; ?>--
+<body>
+  <h1>EMAL</h1>
+  <p>with <i>paragraphs</i></p>
+</body>
