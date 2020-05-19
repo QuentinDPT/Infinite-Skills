6d0a2e0ccac55741459770e4330185da667b33d2
QuentinDPT <quentin@depotter.fr>
2020-05-19 14:09:51 +0200

    [UPDATE] forgot password

--- Views/Mailing.php
+++ Views/Mailing.php
@@ -1,10 +1,17 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>plop</title>
-  </head>
-  <body>
-    <?= $mailContent ?>
-  </body>
-</html>
+
+--PHP-alt-<?php echo $random_hash; ?>
+Content-Type: text/plain; charset="iso-8859-1"
+Content-Transfer-Encoding: 7bit
+
+Hello World!!!
+This is simple text email message.
+
+--PHP-alt-<?php echo $random_hash; ?>
+Content-Type: text/html; charset="iso-8859-1"
+Content-Transfer-Encoding: 7bit
+
+<h2>Hello World!</h2>
+<p>This is something with <b>HTML</b> formatting.</p>
+<?= $mailContent ?>
+
+--PHP-alt-<?php echo $random_hash; ?>--
