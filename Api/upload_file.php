133fdda31b6a69b16332eecac6776b16badd9a09
TRIOREAU MAXIME <maxime.trioreau@etu.unistra.fr>
2020-06-02 11:34:19 +0200

    [UPDATE] - upload_file - location while upload error

--- Api/upload_file.php
+++ Api/upload_file.php
@@ -10,19 +10,19 @@ $fileExtension= strtolower($fileExtension);
 
 
 if (isset($name)) {
-
     $path = $_SERVER['DOCUMENT_ROOT']."/videos/";
     if (!empty($name)){
-        if ($fileExtension !== "mp4" && $fileExtension !== "ogg" && $fileExtension !== "webm")
-        {
+        if ($fileExtension !== "mp4" && $fileExtension !== "ogg" && $fileExtension !== "webm"){
             die();
-        }
-        else{
+        }else{
             $date = date('Y-m-d', time());
             $value = rand(0, 10000);
             $fileName = $date.$value.$name;
             if (move_uploaded_file($tmp_name, $path.$fileName)) {
                 $videoPath = "videos/$fileName";
+            }else{
+                header("Location : /home");
+                die();
             }
         }
     }else{
