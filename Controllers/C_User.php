172ab21375c40daaf5fcb3babe5ad67d44a1b598
QuentinDPT <quentin@depotter.fr>
2020-06-03 11:59:12 +0200

    [FIX] User Forgot password

--- Controllers/C_User.php
+++ Controllers/C_User.php
@@ -82,7 +82,7 @@ class C_User {
 
         // Change password into the db
         $bdd = C_User::GetBdd();
-        $bdd->update("update User set Password = " . $newPassword . " where Id = " . $user->getId(), []);
+        $bdd->update("update User set Password = '" . $newPassword . "' where Id = " . $user->getId(), []);
 
         return new Mail($dest, $sub, $message, $headers) ;
     }
