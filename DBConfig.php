d52ac8fa4ee2fffcbc2ffc9d7bfd8c63b0f664ef
QuentinDPT <quentin@depotter.fr>
2020-06-14 12:05:48 +0200

    [FIX] DBConfig

--- DBConfig.php
+++ DBConfig.php
@@ -1,6 +1,6 @@
 <?php
 $DBConfig = new \stdClass();
-$DBConfig->DBHost = "hi5bu.myd.infomaniak.com" ;
+$DBConfig->DBHost = "localhost:3308" ;
 $DBConfig->DBName = "hi5bu_infinite_skills" ;
-$DBConfig->DBPassword = "hi5bu_infsk" ;
-$DBConfig->DBUser = "tacosdumardi" ;
+$DBConfig->DBPassword = "root" ;
+$DBConfig->DBUser = "" ; 
