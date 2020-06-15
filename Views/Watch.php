08c86d780e5e3938be3259377aa741753cae0a57
RUHLMANN Olivier <ruhlmann.olivier@hotmail.fr>
2020-06-15 16:05:02 +0200

    [FIX] Watch.php - removed player stop after 6s playing

--- Views/Watch.php
+++ Views/Watch.php
@@ -92,7 +92,6 @@ function createVideoFrame($video){
          var done = false;
          function onPlayerStateChange(event) {
            if (event.data == YT.PlayerState.PLAYING && !done) {
-             setTimeout(stopVideo, 6000);
              done = true;
            }
          }
